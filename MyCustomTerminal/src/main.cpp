2c5d8715200340a1038f2cb987e3897d7309a0c6
CodeGabe23 <gabrielcristiandan@hotmail.com>
2025-09-08 20:24:57 +0100

    Added a blacklist
    the blacklist stops certain commands from running and got some error handling

--- MyCustomTerminal/src/main.cpp
+++ MyCustomTerminal/src/main.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
 #include <cstdlib> // for system()
+#include <vector>
+#include <string>
 
 int main(void)
 {
-	std::string userCommand;
-	
-	std::cout << "Enter a command to run: \n";
-	std::getline(std::cin, userCommand);
-
-	int executionResult = system(userCommand.c_str());
-	if (executionResult == -1)
+	std::vector<std::string> BLACKLIST {"rm -rf /", "rm -rf *", "rm -rf ." ":(){:|:&};:", 
+						"wget", "chmod -R 777 /", "echo 't'"};
+		
+	bool finished = false;
+	while (!finished)
 	{
-		std::cerr << "Execution failed for:\n'" << userCommand << "'\n\n";
+
+		std::string userCommand;
+		std::cout << "Enter a command to run: \n";
+		std::getline(std::cin, userCommand);
+		
+		bool accepted = true;
+		int executionResult{};
+
+		for (int i{}; i < BLACKLIST.size(); i++)
+		{
+			if (userCommand == BLACKLIST.at(i))
+			{
+				std::cout << "Command Disallowed. Try again.\n";
+				accepted = false;
+				break; //dont need to loop anymore
+			}
+			
+		}
+
+		if (accepted)
+		{				
+			executionResult = system(userCommand.c_str());
+		}
+
+		if (executionResult == -1)
+		{			
+			std::cerr << "Execution failed for:\n'" << userCommand <<"'\n";
+		}
 	}
+
 
 	return 0;
 }
